5dbbb9fc2c9e0fbf6df95365d534034845d3590f
anujnath <anujnath80111@gmail.com>
2018-02-21 09:18:22 +0200

    Done building ugly home page

---
+++ photos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from airtable import Airtable
+import os
+
+AT=Airtable(os.environ.get('AIRTABLE_PHOTOSTABLE_BASE_ID'),
+                          'Fotograph',
+                          api_key=os.environ.get('AIRTABLE_API_KEY'))
+# Create your views here.
+def home_page(request):
+    user_query=str(request.GET.get('query',''))
+    search_result=AT.get_all(formula="FIND('"+ user_query.lower() +"',LOWER({Caption}))")
+    stuff_for_frontend={'search_result':search_result}
+    return render(request, 'photos/photos_stuff.html', stuff_for_frontend)
