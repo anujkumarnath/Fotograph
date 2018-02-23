56b22218b0e61d850f87deaeb326a7a5f0d2fc86
anujnath <anujnath80111@gmail.com>
2018-02-23 08:05:29 +0000

    Created the add photos backend

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.home_page, name='home_page'),
-    path('add/',views.add, name='add')
+    path('add/',views.add, name='add'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -12,4 +12,14 @@ def home_page(request):
     stuff_for_frontend={'search_result':search_result}
     return render(request, 'photos/photos_stuff.html', stuff_for_frontend)
 def add(request):
+    if request.method=="POST":
+        data={
+            "Caption": request.POST.get("caption"),
+            "Photo" : [{"url":request.POST.get("url")}],
+            "Date" : request.POST.get("date"),
+            "Rating" : int(request.POST.get("rating")),
+            "Comment" : request.POST.get("comment")
+        }
+        print(data)
+        AT.insert(data)
     return redirect('/')
